977ceb6ecb4e7f2ba464933aa6fa95a7d27605e6
Santiago Maradona <pepioca@gmail.com>
2017-01-02 14:37:43 -0300

    Add missing methods to integration lib

--- nodejs/olpays-line/index.js
+++ nodejs/olpays-line/index.js
@@ -1,27 +1,29 @@
 'use strict'
 
-const request = require('request');
+const rp = require('request-promise');
 const base64 = require('base-64');
 var utf8 = require('utf8');
 
-let pubKey,
-    privKey,
+let publicKey,
+    secretKey,
+    apiKeyId,
     olpaysUri;
 
 var init = (params) => {
-    pubKey = params.pubKey;
-    privKey = params.pubKey;
+    apiKeyId = params.apiKeyId;
+    publicKey = params.publicKey;
+    secretKey = params.secretKey;
     olpaysUri = (params.sandbox) ?
         'https://sandbox.olpays.com/api/v1/' :
         'https://olpays.com/api/v1/';
 }
 
 var getPayments = (page, from, to) => {
-    let getUri = olpaysUri + 'payments?page=' + page + '&from=' + form + '&to=' + to;
+    let getUri = olpaysUri + 'payments?page=' + page + '&from=' + from + '&to=' + to;
     var options = {
         uri: getUri,
         headers: {
-            'Authorization': 'Basic ' + base64.encode(utf8.enconde(pubKey + ':' + privKey))
+            'Authorization': 'Basic ' + base64.encode(utf8.encode(apiKeyId + ':' + secretKey))
         },
         json: true // Automatically parses the JSON string in the response
     };
@@ -37,8 +39,99 @@ var getPayments = (page, from, to) => {
 
 }
 
+var getPayment = paymentId => {
+    let getUri = olpaysUri + 'payments/' + paymentId;
+    var options = {
+        uri: getUri,
+        headers: {
+            'Authorization': 'Basic ' + base64.encode(utf8.encode(apiKeyId + ':' + secretKey))
+        },
+        json: true // Automatically parses the JSON string in the response
+    };
+    return new Promise(function (resolve, reject) {
+        rp(options)
+            .then(function (parsedBody) {
+                resolve(parsedBody);
+            })
+            .catch(function (err) {
+                reject(err);
+            });
+    });
+
+}
+
+var createPayment = paymentData => {
+    let getUri = olpaysUri + 'payments';
+    var options = {
+        uri: getUri,
+        method: 'POST',
+        body: paymentData,
+        headers: {
+            'Authorization': 'Basic ' + base64.encode(utf8.encode(apiKeyId + ':' + secretKey))
+        },
+        json: true // Automatically parses the JSON string in the response
+    };
+    return new Promise(function (resolve, reject) {
+        rp(options)
+            .then(function (parsedBody) {
+                resolve(parsedBody);
+            })
+            .catch(function (err) {
+                reject(err);
+            });
+    });
+
+}
+
+var createCardToken = cardData => {
+    let getUri = olpaysUri + 'public/cardtoken';
+    var options = {
+        uri: getUri,
+        method: 'POST',
+        body: cardData,
+        headers: {
+            'Authorization': 'Basic ' + base64.encode(utf8.encode(apiKeyId + ':' + publicKey))
+        },
+        json: true // Automatically parses the JSON string in the response
+    };
+    return new Promise(function (resolve, reject) {
+        rp(options)
+            .then(function (parsedBody) {
+                resolve(parsedBody);
+            })
+            .catch(function (err) {
+                reject(err);
+            });
+    });
+
+}
+
+var getCurrentBusiness = () => {
+    let getUri = olpaysUri + 'business/current';
+    var options = {
+        uri: getUri,
+        headers: {
+            'Authorization': 'Basic ' + base64.encode(utf8.encode(apiKeyId + ':' + secretKey))
+        },
+        json: true // Automatically parses the JSON string in the response
+    };
+    return new Promise(function (resolve, reject) {
+        rp(options)
+            .then(function (parsedBody) {
+                resolve(parsedBody);
+            })
+            .catch(function (err) {
+                reject(err);
+            });
+    });
+
+}
 
 module.exports = {
     getPayments,
+    getPayment,
+    createPayment,
+    createCardToken,
+    getCurrentBusiness,
     init
 }
